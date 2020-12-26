dd3a5848312945845854a020e4e372d23b91acfa
dangdung <dangdung@dangdungs-MacBook-Pro.local>
2020-12-26 10:14:36 +0700

    Build multi platform image

--- build.sh
+++ build.sh
@@ -4,6 +4,5 @@ for i in `find . -name "Dockerfile" -type f`; do
     IMAGE_NAME="${DOCKER_REPO}:${TAG_NAME}"
     CONTEXT=$(echo ${i} | sed 's/\/Dockerfile//')
     echo "Building ${IMAGE_NAME} with Dockerfile ${i}"
-    docker build -t ${IMAGE_NAME} -f ${i} ${CONTEXT}
-    docker push ${IMAGE_NAME}
+    docker buildx build --platform=linux/amd64,linux/arm64/v8 -t ${IMAGE_NAME} -f ${i} --push ${CONTEXT}
 done
