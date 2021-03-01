93d276318fe3adb0b5fdbf308c71af8b6bfc52d1
Dung Nguyen Dang <dangdungcntt@gmail.com>
2021-03-01 12:36:35 +0700

    Build with Gitlab CI

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-DOCKER_REPO="dangdungcntt/php"
+DOCKER_REPO="${DOCKER_REPO:-dangdungcntt/php}"
 for i in `find . -name "Dockerfile" -type f`; do
     TAG_NAME=$(echo ${i} | grep -oE '[a-z0-9\.\-]+\/Dockerfile' | sed 's/\/Dockerfile//')
     IMAGE_NAME="${DOCKER_REPO}:${TAG_NAME}"
     CONTEXT=$(echo ${i} | sed 's/\/Dockerfile//')
     echo "Building ${IMAGE_NAME} with Dockerfile ${i}"
-    docker buildx build --platform=linux/amd64,linux/arm64/v8 -t ${IMAGE_NAME} -f ${i} --push ${CONTEXT}
+    docker build -t ${IMAGE_NAME} -f ${i} ${CONTEXT}
+    docker push ${IMAGE_NAME}
 done
--- phpearth/build.sh
+++ phpearth/build.sh
@@ -3,5 +3,6 @@ for i in `find . -name "*.Dockerfile" -type f`; do
     TAG_NAME=$(echo ${i} | grep -oE '[a-z0-9\.\-]+\.Dockerfile' | sed 's/\.Dockerfile//')
     IMAGE_NAME="${DOCKER_REPO}:${TAG_NAME}"
     echo "Building ${IMAGE_NAME} with Dockerfile ${i}"
-    docker buildx build --platform=linux/amd64,linux/arm64/v8 -t ${IMAGE_NAME} -f ${i} --push .
+    docker build -t ${IMAGE_NAME} -f ${i} .
+    docker push ${IMAGE_NAME}
 done
