1ef1c9dbdac44f1a9186eb98b6560ccd85e1b716
Dung Nguyen Dang <dangdungcntt@gmail.com>
2023-05-22 00:23:51 +0700

    Update docker-entrypoint.sh

--- phpearth/docker-entrypoint.sh
+++ phpearth/docker-entrypoint.sh
@@ -2,22 +2,57 @@
 
 set -e
 
+secret_folder=${SECRET_FOLDER:-"/run/secrets"}
 # Function to read Docker secret file and set environment variable
 read_secret() {
     local secret_file=$1
     local secret_name=${secret_file##*/}
-    local env_var_name=$(echo "$secret_name" | tr '[:lower:]' '[:upper:]')
+    local default_var_name=$(echo "$secret_name" | tr '[:lower:]' '[:upper:]')
+    local env_var_name=${2:-$default_var_name}
 
     if [ -f "$secret_file" ]; then
         export "$env_var_name"=$(cat "$secret_file")
-        echo "Loaded secret $secret_name into $env_var_name"
     fi
 }
 
-# Loop through all secret files in the /run/secrets directory
-for secret_file in /run/secrets/*; do
+# Function to read environment variables mapping to secret names
+mapDockerSecrets() {
+    IFS=',' read -ra map_secrets <<< "$MAP_DOCKER_SECRETS"
+
+    if [ ! -n "$map_secrets" ]; then
+        return
+    fi
+
+    for env_var_name in "${map_secrets[@]}"; do
+        local lower_var_name=$(echo "$env_var_name" | tr '[:upper:]' '[:lower:]')
+
+        if [ -f "$secret_folder/$lower_var_name" ]; then
+            continue
+        fi
+
+        local secret_name=$(eval "echo \$$env_var_name")
+
+        if [ -n "$secret_name" ]; then
+            secret_file="$secret_folder/$secret_name"
+
+            if [ -f "$secret_file" ]; then
+                read_secret "$secret_file" "$env_var_name"
+            else
+                echo "Secret file $secret_file not found!"
+                exit 1
+            fi
+        else
+            echo "No secret name '$secret_name' found for to map into variable $env_var_name"
+            exit 1
+        fi
+    done
+}
+
+for secret_file in $secret_folder/*; do
     read_secret "$secret_file"
 done
+
+mapDockerSecrets
 
 # Run the main command
 exec "$@"
